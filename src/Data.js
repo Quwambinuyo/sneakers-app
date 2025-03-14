68438349052eb8cf39710fc8873afd7d3d266b50
Quwam Binuyo <qbinuyo7@gmail.com>
2025-03-14 08:06:34 +0100

    added the functionality to select items and re render the new image and also giving it a styling

---
+++ src/Data.js
@@ -0,0 +1,46 @@
+import Thumbnail1 from "./assets/images/image-product-1-thumbnail.jpg";
+import Thumbnail2 from "./assets/images/image-product-2-thumbnail.jpg";
+import Thumbnail3 from "./assets/images/image-product-3-thumbnail.jpg";
+import Thumbnail4 from "./assets/images/image-product-4-thumbnail.jpg";
+
+import Sneakers1 from "./assets/images/image-product-1.jpg";
+import Sneakers2 from "./assets/images/image-product-2.jpg";
+import Sneakers3 from "./assets/images/image-product-3.jpg";
+import Sneakers4 from "./assets/images/image-product-4.jpg";
+
+const Data = [
+  {
+    id: 1,
+    name: "Fall Limited Edition Sneakers",
+    images: {
+      img: Sneakers1,
+      thumbnail: Thumbnail1,
+    },
+  },
+  {
+    id: 2,
+    name: "Fall Limited Edition Sneakers",
+    images: {
+      img: Sneakers2,
+      thumbnail: Thumbnail2,
+    },
+  },
+  {
+    id: 3,
+    name: "Fall Limited Edition Sneakers",
+    images: {
+      img: Sneakers3,
+      thumbnail: Thumbnail3,
+    },
+  },
+  {
+    id: 4,
+    name: "Fall Limited Edition Sneakers",
+    images: {
+      img: Sneakers4,
+      thumbnail: Thumbnail4,
+    },
+  },
+];
+
+export default Data;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-import Sneakers1 from "../assets/images/image-product-1.jpg";
-import AddToCart from "../assets/images/icon-cart.svg";
-import Plus from "../assets/images/icon-plus.svg";
-import Minus from "../assets/images/icon-minus.svg";
-import Thumbnail1 from "../assets/images/image-product-1-thumbnail.jpg";
-import Thumbnail2 from "../assets/images/image-product-2-thumbnail.jpg";
-import Thumbnail3 from "../assets/images/image-product-3-thumbnail.jpg";
-import Thumbnail4 from "../assets/images/image-product-4-thumbnail.jpg";
+import { useState } from "react";
+import Data from "../Data";
+import cartIcon from "../assets/images/icon-cart.svg";
+import minuIcon from "../assets/images/icon-minus.svg";
+import plusIcon from "../assets/images/icon-plus.svg";
 
 const Home = () => {
+  const [selectedSneaker, setSelectedSneaker] = useState(Data[0]);
+
   return (
     <section className="flex gap-20 items-center mt-[100px]">
       {/* Image Section */}
@@ -16,78 +14,67 @@ const Home = () => {
         {/* Main Image */}
         <div className="mb-4">
           <img
-            src={Sneakers1}
-            alt="sneakers-1"
+            src={selectedSneaker.images.img}
+            alt={selectedSneaker.name}
             className="w-[290px] h-[290px] object-cover rounded-md"
           />
         </div>
 
         {/* Thumbnails */}
-        <div className="flex gap-2 ">
-          <button className="border-2 border-orange-700 rounded-md">
-            <img
-              src={Thumbnail1}
-              alt=""
-              className="w-16 h-16 rounded-md opacity-50"
-            />
-          </button>
-          <button>
-            <img src={Thumbnail2} alt="" className="w-16 h-16 rounded-md" />
-          </button>
-          <button>
-            <img src={Thumbnail3} alt="" className="w-16 h-16 rounded-md" />
-          </button>
-          <button>
-            <img src={Thumbnail4} alt="" className="w-16 h-16 rounded-md" />
-          </button>
+        <div className="flex gap-2">
+          {Data.map((sneaker) => (
+            <button
+              key={sneaker.id}
+              onClick={() => setSelectedSneaker(sneaker)}
+              className={`border-2 rounded-md transition-opacity duration-200 ${
+                selectedSneaker.id === sneaker.id
+                  ? "border-orange-700 opacity-70"
+                  : "border-transparent"
+              }`}
+            >
+              <img
+                src={sneaker.images.thumbnail}
+                alt={sneaker.name}
+                className="w-16 h-16 rounded-md"
+              />
+            </button>
+          ))}
         </div>
       </div>
 
       {/* Text Section */}
       <div className="max-w-[500px]">
         <p className="text-sm text-gray-500">SNEAKER COMPANY</p>
-        <h1 className="my-4 text-4xl font-bold">
-          Fall Limited Edition Sneakers
-        </h1>
+        <h1 className="my-4 text-4xl font-bold">{selectedSneaker.name}</h1>
         <p className="text-gray-700">
           These low-casual are your perfect casual wear companion. Featuring a
           durable outer rubber sole, they'll withstand everything the weather
           can offer.
         </p>
-        <div className="flex flex-row items-center gap-2">
-          <h3 className="text-2xl font-bold">$125.00</h3>
-          <span className="p-[6px] text-sm rounded-md font-bold text-white bg-black flex items-center justify-center">
+
+        <div className="flex flex-row items-center gap-2 mt-4">
+          <h3 className="text-3xl font-bold">$125.00</h3>
+          <span className="text-sm font-semibold text-white bg-black p-0.5 rounded-sm">
             50%
           </span>
         </div>
-        <p className="font-semibold text-gray-500 line-through">$250</p>
 
-        {/* Quantity Selector and Add to Cart Section */}
-        <div className="flex flex-row items-center gap-3 mt-4">
+        <div className="flex flex-row items-center gap-6 mt-4">
           {/* Quantity Selector */}
-          <div className="inline-flex items-center gap-2 p-2 bg-gray-200 rounded-md">
-            {/* Minus Button */}
-            <button className="flex items-center justify-center p-2">
-              <img src={Minus} alt="minus" />
+          <div className="flex items-center bg-gray-200 p-2 rounded-md w-[100px] justify-between">
+            <button type="button">
+              <img src={minuIcon} alt="minus" />
             </button>
-            {/* Quantity Display */}
-            <h4 className="text-sm font-bold">0</h4>
-            {/* Plus Button */}
-            <button className="flex items-center justify-center p-2">
-              <img src={Plus} alt="plus" />
+            <p className="text-sm font-semibold">0</p>
+            <button type="button">
+              <img src={plusIcon} alt="plus" />
             </button>
           </div>
 
           {/* Add to Cart Button */}
-          <div className="flex items-center gap-2">
-            <button className="flex items-center gap-2 px-4 py-2 font-bold text-black bg-orange-700 rounded-md h-[40px]">
-              <img
-                src={AddToCart}
-                alt="add-to-cart"
-                className="w-4 h-4 text-black"
-              />
-              Add to Cart
-            </button>
+          <div className="flex flex-row items-center gap-2 bg-orange-800 justify-center p-2 rounded-md w-[150px]">
+            <img src={cartIcon} alt="cart" className="w-3 h-3" />
+            <p className="text-sm font-semibold text-black">Add to cart</p>
           </div>
         </div>
       </div>
